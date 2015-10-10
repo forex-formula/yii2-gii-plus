96bfcde89dea2a2156ac1b6ec1a03d64101c9473
Ivan Chistyakov <ivan.chkv@gmail.com>
2015-10-10 07:21:39 +0300

    fix views

--- generators/model/form.php
+++ generators/model/form.php
@@ -1,13 +1,15 @@
 <?php
 
-use yii\gii\plus\widgets\AutoComplete,
-    yii\gii\plus\helpers\Helper;
+use yii\jui\autocomplete\AutoComplete;
+use yii\gii\plus\helpers\Helper;
 
 /* @var $this yii\web\View */
 /* @var $generator yii\gii\plus\generators\model\Generator */
 /* @var $form yii\widgets\ActiveForm */
 
-echo $form->field($generator, 'modelClass')->widget(AutoComplete::classname(), ['source' => Helper::getBaseModelClasses()]);
+echo $form->field($generator, 'modelClass')->widget(AutoComplete::classname(), [
+    'source' => Helper::getBaseModelClasses()
+]);
 echo $form->field($generator, 'newModelClass');
 echo $form->field($generator, 'newQueryClass');
 echo $form->field($generator, 'enableI18N')->checkbox();
--- generators/search/form.php
+++ generators/search/form.php
@@ -1,13 +1,15 @@
 <?php
 
-use yii\gii\plus\widgets\AutoComplete,
-    yii\gii\plus\helpers\Helper;
+use yii\jui\autocomplete\AutoComplete;
+use yii\gii\plus\helpers\Helper;
 
 /* @var $this yii\web\View */
 /* @var $generator yii\gii\plus\generators\search\Generator */
 /* @var $form yii\widgets\ActiveForm */
 
-echo $form->field($generator, 'modelClass')->widget(AutoComplete::classname(), ['source' => Helper::getBaseSearchModelClasses()]);
+echo $form->field($generator, 'modelClass')->widget(AutoComplete::classname(), [
+    'source' => Helper::getBaseSearchModelClasses()
+]);
 echo $form->field($generator, 'newModelClass');
 echo $form->field($generator, 'enableI18N')->checkbox();
 echo $form->field($generator, 'messageCategory');
