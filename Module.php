dc1146f1bccb6acf0dc3478fb552523228e78659
Ivan Chistyakov <ivan.chkv@gmail.com>
2016-01-24 16:07:25 +0300

    add bootstrap()

--- Module.php
+++ Module.php
@@ -2,8 +2,21 @@
 
 namespace yii\gii\plus;
 
+use yii\web\Application as WebApplication;
+use Yii;
 use yii\gii\Module as YiiGiiModule;
 
 class Module extends YiiGiiModule
 {
+
+    /**
+     * @inheritdoc
+     */
+    public function bootstrap($app)
+    {
+        parent::bootstrap($app);
+        if ($app instanceof WebApplication) {
+            $this->setViewPath(Yii::getAlias('@yii/gii/views'));
+        }
+    }
 }
