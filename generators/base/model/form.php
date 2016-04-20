aee313b64fb2134ccfcfad8968681c1cb47e7ba6
Ivan Chistyakov <ivan.chkv@gmail.com>
2016-04-21 02:17:14 +0300

    fix form.php

--- generators/base/model/form.php
+++ generators/base/model/form.php
@@ -11,6 +11,7 @@ use yii\web\JsExpression;
 echo $form->field($generator, 'includeFilter');
 echo $form->field($generator, 'excludeFilter');
 echo $form->field($generator, 'tableName')->widget(AutoComplete::className(), [
+    'options' => ['table_prefix' => $generator->getTablePrefix()],
     'source' => $generator->getTableNameAutoComplete(true)
 ]);
 echo $form->field($generator, 'modelClass');
@@ -23,8 +24,11 @@ echo $form->field($generator, 'baseClass')->widget(AutoComplete::className(), [
 ]);
 echo $form->field($generator, 'db')->dropDownList($generator->getDbListItems());
 echo $form->field($generator, 'useTablePrefix')->checkbox();
-echo $form->field($generator, 'useSchemaName')->checkbox();
-echo $form->field($generator, 'generateRelations')->checkbox();
+echo $form->field($generator, 'generateRelations')->dropDownList([
+    Generator::RELATIONS_NONE => 'No relations',
+    Generator::RELATIONS_ALL => 'All relations',
+    Generator::RELATIONS_ALL_INVERSE => 'All relations with inverse'
+]);
 echo $form->field($generator, 'generateLabelsFromComments')->checkbox();
 echo $form->field($generator, 'generateQuery')->checkbox();
 echo $form->field($generator, 'queryNs')->widget(AutoComplete::className(), [
@@ -36,3 +40,4 @@ echo $form->field($generator, 'queryBaseClass')->widget(AutoComplete::className(
 ]);
 echo $form->field($generator, 'enableI18N')->checkbox();
 echo $form->field($generator, 'messageCategory');
+echo $form->field($generator, 'useSchemaName')->checkbox();
