fb3970500a8c2bfd40d509f6afddbaa69fbd3d02
Ivan Chistyakov <ivan.chkv@gmail.com>
2016-06-04 00:54:57 +0300

    fix migration

--- tests/migrations/m160209_192728_init.php
+++ tests/migrations/m160209_192728_init.php
@@ -20,7 +20,8 @@ class m160209_192728_init extends Migration
             'name' => 'VARCHAR(255) NOT NULL',
             'text' => 'TEXT NOT NULL'
         ], $tableOptions);
-        $this->addForeignKey('post__blog_id', 'post', 'blog_id', 'blog', 'id', 'RESTRICT', 'NO ACTION');
+        $this->createIndex('id-blog_id', 'post', ['id', 'blog_id'], true);
+        $this->addForeignKey('post-blog_id', 'post', 'blog_id', 'blog', 'id', 'RESTRICT', 'NO ACTION');
         // comment
         $this->createTable('comment', [
             'id' => 'INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY',
@@ -29,11 +30,10 @@ class m160209_192728_init extends Migration
             'parent_id' => 'INT UNSIGNED NULL DEFAULT NULL',
             'text' => 'TEXT NOT NULL'
         ], $tableOptions);
-        $this->addForeignKey('comment__blog_id', 'comment', 'blog_id', 'blog', 'id', 'RESTRICT', 'NO ACTION');
-        $this->createIndex('id__blog_id', 'post', ['id', 'blog_id'], true);
-        $this->addForeignKey('comment__post_id__blog_id', 'comment', ['post_id', 'blog_id'], 'post', ['id', 'blog_id'], 'RESTRICT', 'NO ACTION');
-        $this->createIndex('id__blog_id__post_id', 'comment', ['id', 'blog_id', 'post_id'], true);
-        $this->addForeignKey('comment__parent_id__blog_id__post_id', 'comment', ['parent_id', 'blog_id', 'post_id'], 'comment', ['id', 'blog_id', 'post_id'], 'RESTRICT', 'NO ACTION');
+        $this->createIndex('id-blog_id-post_id', 'comment', ['id', 'blog_id', 'post_id'], true);
+        $this->addForeignKey('comment-blog_id', 'comment', 'blog_id', 'blog', 'id', 'RESTRICT', 'NO ACTION');
+        $this->addForeignKey('comment-post_id-blog_id', 'comment', ['post_id', 'blog_id'], 'post', ['id', 'blog_id'], 'RESTRICT', 'NO ACTION');
+        $this->addForeignKey('comment-parent_id-blog_id-post_id', 'comment', ['parent_id', 'blog_id', 'post_id'], 'comment', ['id', 'blog_id', 'post_id'], 'RESTRICT', 'NO ACTION');
         // sequence
         $this->createTable('sequence', [
             'id' => 'INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY',
@@ -41,21 +41,21 @@ class m160209_192728_init extends Migration
             'name' => 'VARCHAR(255) NOT NULL'
         ], $tableOptions);
         $this->createIndex('parent_id', 'sequence', 'parent_id', true);
-        $this->addForeignKey('sequence__parent_id', 'sequence', 'parent_id', 'sequence', 'id', 'RESTRICT', 'NO ACTION');
+        $this->addForeignKey('sequence-parent_id', 'sequence', 'parent_id', 'sequence', 'id', 'RESTRICT', 'NO ACTION');
     }
 
     public function down()
     {
         // sequence
+        $this->dropForeignKey('sequence-parent_id', 'sequence');
         $this->dropTable('sequence');
-        $this->dropForeignKey('sequence__parent_id', 'sequence');
         // comment
-        $this->dropForeignKey('comment__parent_id__blog_id__post_id', 'comment');
-        $this->dropForeignKey('comment__post_id__blog_id', 'comment');
-        $this->dropForeignKey('comment__blog_id', 'comment');
+        $this->dropForeignKey('comment-parent_id-blog_id-post_id', 'comment');
+        $this->dropForeignKey('comment-post_id-blog_id', 'comment');
+        $this->dropForeignKey('comment-blog_id', 'comment');
         $this->dropTable('comment');
         // post
-        $this->dropForeignKey('post__blog_id', 'post');
+        $this->dropForeignKey('post-blog_id', 'post');
         $this->dropTable('post');
         // blog
         $this->dropTable('blog');
