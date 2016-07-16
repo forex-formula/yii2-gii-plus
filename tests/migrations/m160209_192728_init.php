ce651933c45f9bbab7577fb4e3c52703257d66a0
Ivan Chistyakov <ivan.chkv@gmail.com>
2016-07-16 10:00:14 +0300

    fix migration

--- tests/migrations/m160209_192728_init.php
+++ tests/migrations/m160209_192728_init.php
@@ -1,63 +1,59 @@
 <?php
 
-use yii\db\Migration;
+use yii\boost\db\Migration;
 
 class m160209_192728_init extends Migration
 {
 
     public function up()
     {
-        $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
-        // blog
-        $this->createTable('blog', [
-            'id' => 'INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY',
-            'name' => 'VARCHAR(255) NOT NULL',
-        ], $tableOptions);
-        // post
-        $this->createTable('post', [
-            'id' => 'INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY',
-            'blog_id' => 'INT UNSIGNED NOT NULL',
-            'name' => 'VARCHAR(255) NOT NULL',
-            'text' => 'TEXT NOT NULL'
-        ], $tableOptions);
-        $this->createIndex('id-blog_id', 'post', ['id', 'blog_id'], true);
-        $this->addForeignKey('post-blog_id', 'post', 'blog_id', 'blog', 'id', 'RESTRICT', 'NO ACTION');
-        // comment
-        $this->createTable('comment', [
-            'id' => 'INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY',
-            'blog_id' => 'INT UNSIGNED NOT NULL',
-            'post_id' => 'INT UNSIGNED NOT NULL',
-            'parent_id' => 'INT UNSIGNED NULL DEFAULT NULL',
-            'text' => 'TEXT NOT NULL'
-        ], $tableOptions);
-        $this->createIndex('id-blog_id-post_id', 'comment', ['id', 'blog_id', 'post_id'], true);
-        $this->addForeignKey('comment-blog_id', 'comment', 'blog_id', 'blog', 'id', 'RESTRICT', 'NO ACTION');
-        $this->addForeignKey('comment-post_id-blog_id', 'comment', ['post_id', 'blog_id'], 'post', ['id', 'blog_id'], 'RESTRICT', 'NO ACTION');
-        $this->addForeignKey('comment-parent_id-blog_id-post_id', 'comment', ['parent_id', 'blog_id', 'post_id'], 'comment', ['id', 'blog_id', 'post_id'], 'RESTRICT', 'NO ACTION');
-        // sequence
+        $this->createTableWithComment('blog', [
+            'id' => $this->primaryKey()->unsigned(),
+            'name' => $this->string(50)->notNull()->comment('Название')->unique(),
+            'enabled' => $this->boolean()->notNull()->defaultValue(0)->comment('Включено'),
+            'deleted' => $this->boolean()->notNull()->defaultValue(0)
+        ], 'Блог');
+
+        $this->createTableWithComment('post', [
+            'id' => $this->primaryKey()->unsigned(),
+            'blog_id' => $this->integer()->unsigned()->notNull()->comment('Блог'),
+            'name' => $this->string(50)->notNull()->comment('Название')->unique(),
+            'text' => $this->text()->notNull()->comment('Текст'),
+            'enabled' => $this->boolean()->notNull()->defaultValue(0)->comment('Включено'),
+            'deleted' => $this->boolean()->notNull()->defaultValue(0)
+        ], 'Пост');
+        $this->createUnique(null, 'post', ['blog_id', 'name']);
+
+        $this->addForeignKey(null, 'post', 'blog_id', 'blog', 'id', static::RESTRICT, static::NO_ACTION);
+
+        $this->createTableWithComment('comment', [
+            'id' => $this->primaryKey()->unsigned(),
+            'blog_id' => $this->integer()->unsigned()->notNull()->comment('Блог'),
+            'post_id' => $this->integer()->unsigned()->notNull()->comment('Пост'),
+            'parent_id' => $this->integer()->unsigned()->comment('Родительский комментарий'),
+            'text' => $this->text()->notNull()->comment('Текст'),
+            'enabled' => $this->boolean()->notNull()->defaultValue(0)->comment('Включено'),
+            'deleted' => $this->boolean()->notNull()->defaultValue(0)
+        ], 'Комментарий');
+
+        $this->addForeignKey(null, 'comment', 'blog_id', 'blog', 'id', static::RESTRICT, static::NO_ACTION);
+
+        $this->createIndex(null, 'post', ['id', 'blog_id']);
+        $this->addForeignKey(null, 'comment', ['post_id', 'blog_id'], 'post', ['id', 'blog_id'], static::RESTRICT, static::NO_ACTION);
+
+        $this->createIndex(null, 'comment', ['id', 'post_id']);
+        $this->addForeignKey(null, 'comment', ['parent_id', 'post_id'], 'comment', ['id', 'post_id'], static::RESTRICT, static::NO_ACTION);
+
         $this->createTable('sequence', [
-            'id' => 'INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY',
-            'parent_id' => 'INT UNSIGNED NULL DEFAULT NULL',
-            'name' => 'VARCHAR(255) NOT NULL'
-        ], $tableOptions);
-        $this->createIndex('parent_id', 'sequence', 'parent_id', true);
-        $this->addForeignKey('sequence-parent_id', 'sequence', 'parent_id', 'sequence', 'id', 'RESTRICT', 'NO ACTION');
+            'id' => $this->primaryKey()->unsigned(),
+            'previous_id' => $this->integer()->unsigned()->unique()
+        ]);
+
+        $this->addForeignKey(null, 'sequence', 'previous_id', 'sequence', 'id', static::RESTRICT, static::NO_ACTION);
     }
 
     public function down()
     {
-        // sequence
-        $this->dropForeignKey('sequence-parent_id', 'sequence');
-        $this->dropTable('sequence');
-        // comment
-        $this->dropForeignKey('comment-parent_id-blog_id-post_id', 'comment');
-        $this->dropForeignKey('comment-post_id-blog_id', 'comment');
-        $this->dropForeignKey('comment-blog_id', 'comment');
-        $this->dropTable('comment');
-        // post
-        $this->dropForeignKey('post-blog_id', 'post');
-        $this->dropTable('post');
-        // blog
-        $this->dropTable('blog');
+        return false;
     }
 }
