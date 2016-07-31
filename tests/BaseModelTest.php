217a982e357992ad795379e03b213186a8e8596d
Ivan Chistyakov <ivan.chkv@gmail.com>
2016-07-31 23:30:32 +0300

    add testClassExists(), testGetParentClass()

--- tests/BaseModelTest.php
+++ tests/BaseModelTest.php
@@ -7,7 +7,19 @@ use yii\phpunit\TestCase;
 class BaseModelTest extends TestCase
 {
 
-    public function testBundle()
+    public function testClassExists()
     {
+        $this->assertTrue(class_exists('app\models\base\TypeBase'));
+        $this->assertTrue(class_exists('app\models\query\base\TypeQueryBase'));
+        $this->assertTrue(class_exists('app\models\Type'));
+        $this->assertTrue(class_exists('app\models\query\TypeQuery'));
+    }
+
+    public function testGetParentClass()
+    {
+        $this->assertEquals('yii\boost\db\ActiveRecord', get_parent_class('app\models\base\TypeBase'));
+        $this->assertEquals('yii\boost\db\ActiveQuery', get_parent_class('app\models\query\base\TypeQueryBase'));
+        $this->assertEquals('app\models\base\TypeBase', get_parent_class('app\models\Type'));
+        $this->assertEquals('app\models\query\base\TypeQueryBase', get_parent_class('app\models\query\TypeQuery'));
     }
 }
