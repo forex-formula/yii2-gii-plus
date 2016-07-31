f7b25238aa009d7578c0915e27c76d398405b9d7
Ivan Chistyakov <ivan.chkv@gmail.com>
2016-07-31 23:14:52 +0300

    fix m160209_192728_init

--- tests/migrations/m160209_192728_init.php
+++ tests/migrations/m160209_192728_init.php
@@ -7,91 +7,10 @@ class m160209_192728_init extends Migration
 
     public function up()
     {
-        $this->createTableWithComment('blog_type', [
+        $this->createTableWithComment('type', [
             'id' => $this->primaryKey(),
-            'name' => $this->string(50)->notNull()->comment('Название')->unique(),
-        ], 'Тип блога');
-
-        $this->createTableWithComment('blog', [
-            'id' => $this->primaryKey(),
-            'blog_type_id' => $this->integer()->unsigned()->notNull()->comment('Тип блога'),
-            'name' => $this->string(50)->notNull()->comment('Название')->unique(),
-            'enabled' => $this->enabledShortcut()->comment('Включено'),
-            'created_at' => $this->createdAtShortcut()->comment('Создано в'),
-            'updated_at' => $this->updatedAtShortcut()->comment('Обновлено в'),
-            'deleted' => $this->deletedShortcut()
-        ], 'Блог');
-
-        $this->addForeignKey(null, 'blog', ['blog_type_id'], 'blog_type', ['id']);
-
-        $this->createTableWithComment('post', [
-            'id' => $this->primaryKey(),
-            'blog_id' => $this->integer()->unsigned()->notNull()->comment('Блог'),
-            'name' => $this->string(50)->notNull()->comment('Название'),
-            'text' => $this->text()->notNull()->comment('Текст'),
-            'enabled' => $this->enabledShortcut()->comment('Включено'),
-            'created_at' => $this->createdAtShortcut()->comment('Создано в'),
-            'updated_at' => $this->updatedAtShortcut()->comment('Обновлено в'),
-            'deleted' => $this->deletedShortcut()
-        ], 'Пост');
-        $this->createUnique(null, 'post', ['blog_id', 'name']);
-
-        $this->addForeignKey(null, 'post', ['blog_id'], 'blog', ['id']);
-
-        $db = $this->getDb();
-        $sql = <<<SQL
-CREATE VIEW `post_report` AS
-SELECT *
-FROM `post`;
-SQL;
-        $db->createCommand($sql)->execute();
-
-        $this->createTableWithComment('comment', [
-            'id' => $this->primaryKey(),
-            'post_id' => $this->integer()->unsigned()->notNull()->comment('Пост'),
-            'parent_id' => $this->integer()->unsigned()->comment('Родительский комментарий'),
-            'text' => $this->text()->notNull()->comment('Текст'),
-            'enabled' => $this->enabledShortcut()->comment('Включено'),
-            'created_at' => $this->createdAtShortcut()->comment('Создано в'),
-            'updated_at' => $this->updatedAtShortcut()->comment('Обновлено в'),
-            'deleted' => $this->deletedShortcut()
-        ], 'Комментарий');
-
-        $this->addForeignKey(null, 'comment', ['post_id'], 'post', ['id']);
-
-        $this->createIndex(null, 'comment', ['id', 'post_id']);
-        $this->addForeignKey(null, 'comment', ['parent_id', 'post_id'], 'comment', ['id', 'post_id']);
-
-        $sql = <<<SQL
-CREATE VIEW `comment_report` AS
-SELECT *
-FROM `comment`;
-SQL;
-        $db->createCommand($sql)->execute();
-
-        $this->createTable('sequence', [
-            'id' => $this->primaryKey(),
-            'previous_id' => $this->integer()->unsigned()->unique(),
-            'value' => $this->money()->unsigned(),
-            'value_expires_at' => $this->timestamp()->null()
-        ]);
-
-        $this->addForeignKey(null, 'sequence', 'previous_id', 'sequence', 'id');
-
-        $this->createTable('test', [
-            'small_id' => $this->smallInteger()->unsigned(),
-            'tiny_id' => $this->tinyInteger()->unsigned()
-        ]);
-        $this->addPrimaryKey(null, 'test', ['small_id', 'tiny_id']);
-
-        $sql = <<<SQL
-CREATE VIEW `test_report` AS
-SELECT
-    t.small_id AS pk_small_id,
-    t.tiny_id AS pk_tiny_id
-FROM `test` t;
-SQL;
-        $db->createCommand($sql)->execute();
+            'name' => $this->string(50)->notNull()->comment('Название')->unique()
+        ], 'Тип');
     }
 
     public function down()
