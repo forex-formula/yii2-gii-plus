a4401137fdcc5a8161f38a60362c33e4281a8494
Ivan Chistyakov <ivan.chkv@gmail.com>
2016-08-20 15:56:37 +0300

    fix migration

--- tests/migrations/m160209_192728_init.php
+++ tests/migrations/m160209_192728_init.php
@@ -20,7 +20,11 @@ class m160209_192728_init extends Migration
         $this->createTableWithComment('folder', [
             'id' => $this->primaryKey(),
             'type_id' => $this->tinyInteger()->unsigned()->notNull()->comment('Тип'),
-            'name' => $this->string(50)->notNull()->comment('Название')
+            'name' => $this->string(50)->notNull()->comment('Название'),
+            'visible' => $this->boolean()->notNull()->defaultValue(1)->comment('Видимый'),
+            'created_at' => $this->createdAtShortcut()->comment('Создано в'),
+            'updated_at' => $this->updatedAtShortcut()->comment('Обновлено в'),
+            'deleted' => $this->deletedShortcut()
         ], 'Папка');
         $this->createUnique(null, 'folder', ['type_id', 'name']);
 
@@ -30,6 +34,7 @@ class m160209_192728_init extends Migration
             'id' => $this->primaryKey(),
             'folder_id' => $this->integer()->unsigned()->notNull()->comment('Папка'),
             'name' => $this->string(50)->notNull()->comment('Название'),
+            'visible' => $this->boolean()->notNull()->defaultValue(1)->comment('Видимый'),
             'created_at' => $this->createdAtShortcut()->comment('Создано в'),
             'updated_at' => $this->updatedAtShortcut()->comment('Обновлено в'),
             'deleted' => $this->deletedShortcut()
