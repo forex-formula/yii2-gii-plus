4dcd4fbfbea74ba8f96c01c6d687e6bcc114973b
Ivan Chistyakov <ivan.chkv@gmail.com>
2016-09-24 19:12:35 +0300

    fix FixtureTest

--- test-app/tests/FixtureTest.php
+++ test-app/tests/FixtureTest.php
@@ -60,4 +60,78 @@ class FixtureTest extends TestCase
     {
         static::assertEquals('yii\boost\test\ActiveFixture', get_parent_class('app\fixtures\\' . $fixtureName));
     }
+
+    /**
+     * @param string $fixtureName
+     * @dataProvider fixtureNameDataProvider
+     */
+    public function testModelClass($fixtureName)
+    {
+        /* @var $fixtureClass string|\yii\boost\test\ActiveFixture */
+        $fixtureClass = 'app\fixtures\\' . $fixtureName;
+        $fixture = new $fixtureClass;
+        static::assertEquals('app\models\\' . $fixtureName, $fixture->modelClass);
+    }
+
+    /**
+     * @return array
+     */
+    public function dependsDataProvider()
+    {
+        return [ // [$fixtureName, $depends]
+            ['RootFolder', []],
+            ['Folder', []],
+            ['File', []],
+            ['FileInfo', []],
+            ['Sequence', []],
+            ['Something', []]
+        ];
+    }
+
+    /**
+     * @param string $fixtureName
+     * @param string[] $depends
+     * @dataProvider dependsDataProvider
+     */
+    public function testDepends($fixtureName, $depends)
+    {
+        $fixtureClass = 'app\fixtures\\' . $fixtureName;
+        /* @var $fixture \yii\boost\test\ActiveFixture */
+        $fixture = new $fixtureClass;
+        static::assertCount(count($depends), $fixture->depends);
+        foreach ($depends as $depend) {
+            static::assertContains($depend, $fixture->depends);
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function backDependsDataProvider()
+    {
+        return [ // [$fixtureName, $backDepends]
+            ['RootFolder', []],
+            ['Folder', []],
+            ['File', []],
+            ['FileInfo', []],
+            ['Sequence', []],
+            ['Something', []]
+        ];
+    }
+
+    /**
+     * @param string $fixtureName
+     * @param string[] $backDepends
+     * @dataProvider backDependsDataProvider
+     */
+    public function testBackDepends($fixtureName, $backDepends)
+    {
+        $fixtureClass = 'app\fixtures\\' . $fixtureName;
+        /* @var $fixture \yii\boost\test\ActiveFixture */
+        $fixture = new $fixtureClass;
+        static::assertCount(count($backDepends), $fixture->backDepends);
+        foreach ($backDepends as $backDepend) {
+            static::assertContains($backDepend, $fixture->backDepends);
+        }
+    }
 }
